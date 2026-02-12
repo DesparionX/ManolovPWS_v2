62953783aa1afe448cfc6a36022f295a92699386
DesparionX <dmanolovx@gmail.com>
2026-02-12 16:17:17 +0200

    Implemented ProjectFactory.

---
+++ ManolovPWS_v2.Infrastructure/Contracts/Factories/ProjectFactory.cs
@@ -0,0 +1,23 @@
+﻿using ManolovPWS_v2.Domain.Contracts.Factories;
+using ManolovPWS_v2.Domain.Models.Project;
+using ManolovPWS_v2.Infrastructure.Contracts.Maps;
+using ManolovPWS_v2.Infrastructure.Persistance;
+
+namespace ManolovPWS_v2.Infrastructure.Contracts.Factories
+{
+    public sealed class ProjectFactory(AppDbContext context) : IProjectFactory
+    {
+        private readonly AppDbContext _context = context;
+
+        public async Task<Project?> CreateAsync(Project project, CancellationToken cancellationToken = default)
+        {
+            var entity = project.ToDbEntity();
+
+            _context.Projects.Add(entity);
+
+            var result = await _context.SaveChangesAsync(cancellationToken);
+
+            return result > 0 ? entity.ToDomain() : default;
+        }
+    }
+}
