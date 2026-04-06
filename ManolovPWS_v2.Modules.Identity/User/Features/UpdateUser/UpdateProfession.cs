fa4159057aae5c447b16c56b5f05d469fb654e52
DesparionX <dmanolovx@gmail.com>
2026-04-06 23:24:10 +0300

    Created UpdateProfession command with handler.

---
+++ ManolovPWS_v2.Modules.Identity/User/Features/UpdateUser/UpdateProfession.cs
@@ -0,0 +1,33 @@
+﻿using ManolovPWS_v2.Domain.Contracts.Repositories;
+using ManolovPWS_v2.Domain.Models.User.Properties;
+using ManolovPWS_v2.Modules.Identity.Results;
+using ManolovPWS_v2.Shared.Abstractions.CQRS;
+using ManolovPWS_v2.Shared.Abstractions.Identity;
+
+namespace ManolovPWS_v2.Modules.Identity.User.Features.UpdateUser
+{
+    public sealed record UpdateProfessionCommand(string Profession) : ICommand<IdentityAppResult>;
+    
+    public sealed class UpdateProfessionCommandHandler(IUserRepository userRepository, ICurrentUser<UserId> currentUser)
+        : ICommandHandler<UpdateProfessionCommand, IdentityAppResult>
+    {
+        private readonly IUserRepository _userRepository = userRepository;
+        private readonly ICurrentUser<UserId> _currentUser = currentUser;
+
+        public async Task<IdentityAppResult> HandleAsync(UpdateProfessionCommand command, CancellationToken cancellationToken = default)
+        {
+            var user = await _userRepository.FindByIdAsync(_currentUser.Id, cancellationToken);
+
+            var newProfession = Profession.Create(command.Profession);
+
+            var updated = user.UpdateProfession(newProfession);
+
+            var result = await _userRepository.SaveAsync(updated, cancellationToken);
+
+            return (IdentityAppResult)(result.IsSuccess
+                ? IdentityAppResults.Success()
+                : IdentityAppResults.Failure(result.Errors!));
+        }
+    }
+
+}
