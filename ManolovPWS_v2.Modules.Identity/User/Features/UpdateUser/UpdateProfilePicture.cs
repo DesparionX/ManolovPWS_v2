4e15b13161bb77abe285b2da3cbaf5cbb4097a67
DesparionX <dmanolovx@gmail.com>
2026-04-11 22:53:48 +0300

    Created update profile picture feature.

---
+++ ManolovPWS_v2.Modules.Identity/User/Features/UpdateUser/UpdateProfilePicture.cs
@@ -0,0 +1,32 @@
+﻿using ManolovPWS_v2.Domain.Contracts.Repositories;
+using ManolovPWS_v2.Domain.Models.User.Properties;
+using ManolovPWS_v2.Modules.Identity.Results;
+using ManolovPWS_v2.Shared.Abstractions.CQRS;
+using ManolovPWS_v2.Shared.Abstractions.Identity;
+
+namespace ManolovPWS_v2.Modules.Identity.User.Features.UpdateUser
+{
+    public sealed record UpdateProfilePictureCommand(string NewUrl) : ICommand<IdentityAppResult>;
+
+    public sealed class UpdateProfilePictureCommandHandler(IUserRepository userRepository, ICurrentUser<UserId> currentUser)
+        : ICommandHandler<UpdateProfilePictureCommand, IdentityAppResult>
+    {
+        private readonly IUserRepository _userRepository = userRepository;
+        private readonly ICurrentUser<UserId> _currentUser = currentUser;
+
+        public async Task<IdentityAppResult> HandleAsync(UpdateProfilePictureCommand command, CancellationToken cancellationToken = default)
+        {
+            var user = await _userRepository.FindByIdAsync(_currentUser.Id, cancellationToken);
+
+            var newProfilePicture = ProfilePicture.Create(command.NewUrl);
+
+            var updated = user.UpdateProfilePicture(newProfilePicture);
+
+            var result = await _userRepository.SaveAsync(updated, cancellationToken);
+
+            return (IdentityAppResult)(result.IsSuccess
+                ? IdentityAppResults.Success()
+                : IdentityAppResults.Failure(result.Errors!));
+        }
+    }
+}
