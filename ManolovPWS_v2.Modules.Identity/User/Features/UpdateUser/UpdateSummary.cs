98396392972b140189c2db84be64fdaaf912f1dc
DesparionX <dmanolovx@gmail.com>
2026-04-11 22:34:44 +0300

    Created update summary feature.

---
+++ ManolovPWS_v2.Modules.Identity/User/Features/UpdateUser/UpdateSummary.cs
@@ -0,0 +1,32 @@
+﻿using ManolovPWS_v2.Domain.Contracts.Repositories;
+using ManolovPWS_v2.Domain.Models.User.Properties;
+using ManolovPWS_v2.Modules.Identity.Results;
+using ManolovPWS_v2.Shared.Abstractions.CQRS;
+using ManolovPWS_v2.Shared.Abstractions.Identity;
+
+namespace ManolovPWS_v2.Modules.Identity.User.Features.UpdateUser
+{
+    public sealed record UpdateSummaryCommand(string Summary) : ICommand<IdentityAppResult>;
+
+    public sealed class UpdateSummaryCommandHandler(IUserRepository userRepository, ICurrentUser<UserId> currentUser)
+        : ICommandHandler<UpdateSummaryCommand, IdentityAppResult>
+    {
+        private readonly IUserRepository _userRepository = userRepository;
+        private readonly ICurrentUser<UserId> _currentUser = currentUser;
+
+        public async Task<IdentityAppResult> HandleAsync(UpdateSummaryCommand command, CancellationToken cancellationToken = default)
+        {
+            var user = await _userRepository.FindByIdAsync(_currentUser.Id, cancellationToken);
+
+            var newSummary = Summary.Create(command.Summary);
+
+            var updated = user.UpdateSummary(newSummary);
+
+            var result = await _userRepository.SaveAsync(updated, cancellationToken);
+
+            return (IdentityAppResult)(result.IsSuccess
+                ? IdentityAppResults.Success()
+                : IdentityAppResults.Failure(result.Errors!));
+        }
+    }
+}
