82d51a63bc2187ea2f7be954d4133a52b8c01f3b
DesparionX <dmanolovx@gmail.com>
2026-04-22 17:37:28 +0300

    Updated packages.

---
+++ ManolovPWS_v2.Modules.Identity/User/Features/UpdateUser/UpdateContacts.cs
@@ -0,0 +1,25 @@
+﻿using ManolovPWS_v2.Domain.Contracts.Repositories;
+using ManolovPWS_v2.Domain.Models.User.Properties;
+using ManolovPWS_v2.Domain.Models.User.Properties.Contacts;
+using ManolovPWS_v2.Modules.Identity.Results;
+using ManolovPWS_v2.Shared.Abstractions.CQRS;
+using ManolovPWS_v2.Shared.Abstractions.Identity;
+
+namespace ManolovPWS_v2.Modules.Identity.User.Features.UpdateUser
+{
+    public sealed record UpdateContactsCommand(IEnumerable<Shared.SharedProperties.Contact> NewContacts) : ICommand<IdentityAppResult>;
+
+    public sealed class UpdateContactsCommandHandler(IUserRepository userRepository, ICurrentUser<UserId> currentUser)
+        : ICommandHandler<UpdateContactsCommand, IdentityAppResult>
+    {
+        private readonly IUserRepository _userRepository = userRepository;
+        private readonly ICurrentUser<UserId> _currentUser = currentUser;
+
+        public async Task<IdentityAppResult> HandleAsync(UpdateContactsCommand command, CancellationToken cancellationToken = default)
+        {
+            var user = await _userRepository.FindByIdAsync(_currentUser.Id, cancellationToken);
+
+            var newContacts = Contacts.Create();
+        }
+    }
+}
