e8addf1ea433cad5898b99e60ec73de523360114
DesparionX <dmanolovx@gmail.com>
2026-05-01 21:54:49 +0300

    Created DeleteUser feature.

---
+++ ManolovPWS_v2.Modules.Identity/User/Features/DeleteUser/DeleteUser.cs
@@ -0,0 +1,25 @@
+﻿using ManolovPWS_v2.Domain.Contracts.Repositories;
+using ManolovPWS_v2.Domain.Models.User.Properties;
+using ManolovPWS_v2.Shared.Abstractions.CQRS;
+using ManolovPWS_v2.Shared.Abstractions.Results;
+
+namespace ManolovPWS_v2.Modules.Identity.User.Features.DeleteUser
+{
+    public sealed record DeleteUserCommand(string UserId) : ICommand<ITaskResult>;
+
+    public sealed class DeleteUserCommandHandler(IUserRepository userRepository) 
+        : ICommandHandler<DeleteUserCommand, ITaskResult>
+    {
+        private readonly IUserRepository _userRepository = userRepository;
+        public async Task<ITaskResult> HandleAsync(DeleteUserCommand command, CancellationToken cancellationToken = default)
+        {
+            var userId = UserId.From(command.UserId);
+
+            var result = await _userRepository.RemoveAsync(userId,  cancellationToken);
+
+            return result.IsSuccess ?
+                Result.Success()
+                : Result.Failure(result.Errors);
+        }
+    }
+}
